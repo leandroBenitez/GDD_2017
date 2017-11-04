0fef4ab9559304e245217844de98ba61c3396f00
Fabrizio <fabriziomcrivella@hotmail.com>
2017-11-04 18:57:52 -0300

    procedures
    se actualizan procedures

--- Base de datos/Build DER.sql
+++ Base de datos/Build DER.sql
@@ -891,6 +891,7 @@ create procedure topPorcentajePago(@fechaInicio date, @fechaFin date) as
 		inner join PAGO_AGIL.Lk_Cliente as clie on clie.Cliente_Id = fact.Factura_Cliente_Id
 		inner join PAGO_AGIL.RL_PagoxFactura as pagofact on (fact.Factura_Id = pagofact.Id_Factura AND pagofact.Id_Pago is not null)
 		where convert(date,fact.Factura_Fecha_Alta) between @fechaInicio and @fechaFin
+		order by porcentaje_pago desc
 	end
 go
 ;
@@ -908,6 +909,7 @@ create procedure topCantidadPagos(@fechaInicio date, @fechaFin date) as
 		inner join PAGO_AGIL.Lk_Cliente as CLIE on FACT.Factura_Cliente_Id = CLIE.Cliente_Id
 		inner join PAGO_AGIL.Ft_Pago as PAGO on PAGOF.Id_Pago = PAGO.Pago_Id
 		where convert(date,PAGO.Pago_Fecha) between @fechaInicio and @fechaFin
+		order by cantidad_pagos desc
 	end
 go
 ;
